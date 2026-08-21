7ed74596ab2f819b6cb6f968af4e124fdc008fe7
Sumukha <sumukhadhandapani@gmail.com>
2026-08-21 16:10:41 +0530

    chore: init vite react tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return <h1 className="text-3xl font-bold text-blue-600">Tailwind works</h1>
+}
+export default App
