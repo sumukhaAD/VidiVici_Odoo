116d3f84fac57e6df2c35db052db515c733c4890
nC5-cloud <1609Harshith@gmail.com>
2026-08-22 12:17:11 +0530

    Supabase Client Updated

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://warwscriycrmnnypmcka.supabase.co';
-const supabaseAnonKey = 'sb_publishable_BmYlJqNsOH82IGxHOaUSeQ_-hxKXmHA';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Ensure this URL is set in your environment variables
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY; // Ensure this key is set in your environment variables
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
